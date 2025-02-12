9174f93a3b7dfc471176385f82710ba7e5af9a43
Conor Driscoll <cdriscoll40@gmail.com>
2025-02-11 17:54:14 -0800

    introduction to collections exercises

---
+++ introduction_to_collections/exercise10.py
@@ -0,0 +1,5 @@
+# names = { 'Chris', 'Clare', 'Karis', 'Karl',
+#           'Max', 'Nick', 'Victor' }
+# print(names)
+
+# This code probably won't print the names in alphabetical order, because the collection "names" is a set, and a set is an unordered collection.
---
+++ introduction_to_collections/exercise11.py
@@ -0,0 +1,10 @@
+nationalities_dict = {
+    'Alice':     'USA',
+    'Francois':  'Canada',
+    'Inti':      'Peru',
+    'Monika':    'Germany',
+    'Sanyu':     'Uganda',
+    'Yoshitaka': 'Japan',
+}
+
+print(nationalities_dict['Alice'])
